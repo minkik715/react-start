ae72c1b6ccd51a517b50579eb04adf6ce07e3178
rlaalsrl715 <minkik715@gmail.com>
2023-06-06 17:45:56 +0900

    feat: arr state 추가 삭제

--- src/App.js
+++ src/App.js
@@ -1,17 +1,46 @@
 import React, {useState} from 'react';
 import "App.css"
-function App() {
 
-    const [fruits, setFruits] = useState(["감자", "고구마", '오이', '가지', "옥수수"]);
+function App() {
+    const [users, setUsers] = useState([]);
+    const [name, setName] = useState("");
+    const [age, setAge] = useState("");
     return (
-        <div className="fruits-style">
-            {
-                fruits.map(fruit => {
-                    return <div className="fruit-border">
-                        {fruit}
-                    </div>
-                })
+        <div>
+            <div>
+                이름 : <input value={name} onChange={
+                event => {
+                    setName(event.target.value)
+                }
+            }/>
+                <br/>
+                나이: <input value={age} onChange={
+                event => {
+                    setAge(event.target.value)
+                }
             }
+            />
+                <button onClick={() => {
+                    setUsers([...users, {"name": name, "age": age, "id": users.length + 1}])
+                    setAge(0)
+                    setName("")
+                }}>등록
+                </button>
+            </div>
+            <div className="fruits-style">
+                {users.map((user) => {
+                    return <div key={user.id} className="fruit-border">
+                        {user.name}-{user.age}-{user.id}
+                        <button onClick={ () => {
+                            setUsers(users.filter((u) => {
+                                return u.id !== user.id
+                            }))
+                        }
+                        }>삭제
+                        </button>
+                    </div>
+                })}
+            </div>
         </div>
 
     );
