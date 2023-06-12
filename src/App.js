633dbc8bddd0f7a2eeec7c8e51f486de6df9728a
rlaalsrl715 <minkik715@gmail.com>
2023-06-12 23:21:50 +0900

    feat: hook

--- src/App.js
+++ src/App.js
@@ -1,41 +1,18 @@
 import React from 'react';
-import styled from "styled-components";
-import TestPage from "./component/TestPage";
-import GlobalStyle from "./GlobalStyle";
-const StBox = styled.div`
-  width: 100px;
-  height: 100px;
-  border: 1px solid ${(props) => props.borderColor};
-  margin: 20px;
-  background-color: ${(props) => props.backGrooundColor} ;
-`
-const StContainer = styled.div`
-display: flex`
 
-const StP = styled.p`
-  color: blue;
-`
+//useEffect: 렌더링 될때, 특정한 작업을 수행해야 할 때 설정하는 훅 (컴포넌트가 화면에 보여졌을떄, 사라졌을 때 )
+//의존 배열 -> 해당 배열에 의존할 스테이트를 넣어주면 해당 스테이트가 변할때만 수행됨
+//clean up -> retrun () => {}
 
-const boxColors = ['red', 'blue', 'green']
+//useRef -> dom에 직접 접근할때 dom의 값을 저장하여 변수로 사용할 때 사용한다.
+// unmount 될 때까지 계속 살아남음
+// 저장 공간 -> ref 변화가 일어나도 렌더링 되지 않음
+// -> stat는 리렌더링이 꼭 필요한 값을 다룰 때 쓰면 되고
+// -> ref는 렌더링을 발생시키지 않는 값을 저장할 때 사용한다.
 
-const getBoxName = (color) => {
-    switch (color) {
-        case "red":
-            return "빨강방스"
-        case "blue":
-            return "파랑박스"
-        case "green":
-            return "초록박스"
-    }
-}
-
-
-function App(p) {
+function App(props) {
     return (
-        <div>
-            <GlobalStyle/>
-            <TestPage title={"바보"} content={"멍청이"}/>
-        </div>
+        <div></div>
     );
 }
 
